78b0b3991a3938b7bf4706f2e2824c69742773fd
Kent Jasper Cabunoc Sisi <2081367@g.cu.edu.ph>
2024-03-29 04:18:42 -0700

    Initial upload

---
+++ run.py
@@ -0,0 +1,19 @@
+import re, requests
+
+
+
+response = requests.request("GET", "https://mullvad.net/en/servers")
+
+      
+# Find all matches
+matches = re.findall(r'socks_name:\".*?\"', response.text)
+
+extracted_texts_with_port = [text.split('"')[1] + ':1080' for text in matches]
+
+print(extracted_texts_with_port)
+
+
+
+with open('result.txt', 'w') as file:
+    for item in extracted_texts_with_port:
+        file.write(f"{item}\n")
